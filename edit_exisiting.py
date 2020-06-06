7c12275f8f02d4a494108560ee0a6189cc6ff59a
rahulshahofficial <65654547+rahulshahofficial@users.noreply.github.com>
2020-06-06 15:53:13 +0530

    Update edit_exisiting.py

--- edit_exisiting.py
+++ edit_exisiting.py
@@ -1,9 +1,7 @@
 import os
 import shutil
 from distutils.dir_util import copy_tree
-print(os.getcwd())
 os.chdir('C:/Users/AnOrgasm/Desktop/TestFolder1')
-print(os.getcwd())
 print(os.listdir())
 Length1 = len(os.listdir('C:/Users/AnOrgasm/Desktop/TestFolder1'))
 Length2 = len(os.listdir('C:/Users/AnOrgasm/Desktop/TestFolder2'))
@@ -13,14 +11,14 @@ for item in os.listdir():
         if os.path.isfile(item) is True:
             shutil.copy(item, 'C:/Users/AnOrgasm/Desktop/TestFolder2')
             Length2 += 1
-            print("{}% copying done".format(Length2/Length1))
+            print("{:.2f}% copying done".format((Length2/Length1)*100))
             print(item)
         else:
             fromDirectory = 'C:/Users/AnOrgasm/Desktop/TestFolder1'
             toDirectory = 'C:/Users/AnOrgasm/Desktop/TestFolder2'
             copy_tree(fromDirectory, toDirectory)
             Length2 += 1
-            print("{}% copying done".format(Length2/Length1))
+            print("{:.2f}% copying done".format((Length2/Length1)*100))
             print(item)
 
 
