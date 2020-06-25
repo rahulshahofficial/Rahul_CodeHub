dc4faf4348736634cb516b9d10c55d5504800a46
rahulshahofficial <65654547+rahulshahofficial@users.noreply.github.com>
2020-06-25 16:27:49 +0530

    Update edit_exisiting.py

--- edit_exisiting.py
+++ edit_exisiting.py
@@ -1,24 +1,58 @@
 import os
 import shutil
 from distutils.dir_util import copy_tree
-os.chdir('C:/Users/AnOrgasm/Desktop/TestFolder1')
+originaldirectory = 'C:/Users/AnOrgasm/Desktop/TestFolder1/'
+os.chdir(originaldirectory)
 print(os.listdir())
-Length1 = len(os.listdir('C:/Users/AnOrgasm/Desktop/TestFolder1'))
-Length2 = len(os.listdir('C:/Users/AnOrgasm/Desktop/TestFolder2'))
-
-for item in os.listdir():
-    if item not in os.listdir('C:/Users/AnOrgasm/Desktop/TestFolder2'):
+sum = 0
+def FolderSize(folder):
+    global sum
+    for item in os.listdir(folder):
         if os.path.isfile(item) is True:
-            shutil.copy(item, 'C:/Users/AnOrgasm/Desktop/TestFolder2')
-            Length2 += 1
-            print("{:.2f}% copying done".format((Length2/Length1)*100))
-            print(item)
-        else:
-            fromDirectory = 'C:/Users/AnOrgasm/Desktop/TestFolder1'
-            toDirectory = 'C:/Users/AnOrgasm/Desktop/TestFolder2'
-            copy_tree(fromDirectory, toDirectory)
-            Length2 += 1
-            print("{:.2f}% copying done".format((Length2/Length1)*100))
-            print(item)
+            sum = sum + os.path.getsize(item)
+    print(sum)
+    for item in os.listdir(folder):
+        if os.path.isdir(item) is True:
+            print(folder + str(item))
+            FolderSize((folder + str(item)))
+    return sum
+
+FolderSize('C:/Users/AnOrgasm/Desktop/TestFolder1/')
+
+os.chdir('C:/Users/AnOrgasm/Desktop/TestFolder1/')
+def copyfile(folder, location):
+    for item in os.listdir(folder):
+        if item not in os.listdir(location):
+            if os.path.isfile(item) is True:
+                print(folder)
+                print(location)
+                print("copying file")
+                shutil.copy(item, location)
+                # print("{:.2f}% copying done".format((Length2/Length1)*100))
+                print(item)
+def copyfolder(folder, location):
+    for item in os.listdir(folder):
+        if item not in os.listdir(location):
+            if os.path.isdir(item) is True:
+                print(folder)
+                print("copying folder")
+                os.mkdir(location + str(item))
+                print((folder + str(item) + "/"))
+                print((location + str(item) + "/"))
+                copyfile((folder + str(item) + "/"), (location + str(item)+"/"))
+
+
+copyfile(('C:/Users/AnOrgasm/Desktop/TestFolder1/'), ('C:/Users/AnOrgasm/Desktop/TestFolder2/'))
+copyfolder(('C:/Users/AnOrgasm/Desktop/TestFolder1/'), ('C:/Users/AnOrgasm/Desktop/TestFolder2/'))
+
+
+        # else:
+        #     fromDirectory = 'C:/Users/AnOrgasm/Desktop/TestFolder1'
+        #     toDirectory = 'C:/Users/AnOrgasm/Desktop/TestFolder2'
+        #     print("copying directory")
+        #     copy_tree(fromDirectory, toDirectory)
+        #     Length2 += 1
+        #     print("{:.2f}% copying done".format((Length2/Length1)*100))
+        #     print(item)
 
 
